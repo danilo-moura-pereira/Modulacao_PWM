403d1b540a21642e281c8bff3dff51f31142b719
Danilo Moura Pereira <danilo.mourapereira@gmail.com>
2025-02-09 00:48:39 -0300

    Inclui o controle do LED RGB AZUL

--- Modulacao_PWM.c
+++ Modulacao_PWM.c
@@ -19,18 +19,23 @@ uint16_t dutyCycle = 200;
 void init_pwm() {
     // Habilita o pino GPIO como PWM do servo motor 
     gpio_set_function(GPIO_PWM_SERVO, GPIO_FUNC_PWM);
+    gpio_set_function(GPIO_PWM_LED, GPIO_FUNC_PWM);
     
     // Captura o canal (slice) PWM do servo motor 
     uint slicePWM = pwm_gpio_to_slice_num(GPIO_PWM_SERVO);
+    uint slicePWMLed = pwm_gpio_to_slice_num(GPIO_PWM_LED);
 
     // Define o dividor de clock do PWM do servo motor em 2.0
     pwm_set_clkdiv(slicePWM, PWM_DIVISER);
+    pwm_set_clkdiv(slicePWMLed, PWM_DIVISER);
 
     // Define o valor WRAP em 2000
     pwm_set_wrap(slicePWM, WRAP_PERIOD);
+    pwm_set_wrap(slicePWMLed, WRAP_PERIOD);
 
     // Habilita o PWM no slice correspondente
     pwm_set_enabled(slicePWM, 1);
+    pwm_set_enabled(slicePWMLed, 1);
 }
 
 // Movimenta suavemente o servo entre dois pulsos, com incremento de 5us
@@ -39,12 +44,14 @@ void move_servo(uint16_t pulso_inicial, uint16_t pulso_final) {
     if (pulso_inicial < pulso_final) {
         while (pulso_inicial <= pulso_final) {
             pwm_set_gpio_level(GPIO_PWM_SERVO, pulso_inicial); 
+            pwm_set_gpio_level(GPIO_PWM_LED, pulso_inicial); 
             pulso_inicial += step;
             sleep_ms(10);  // Delay de 10ms para suavizar a movimentação
         }
     } else {
         while (pulso_inicial >= pulso_final) {
             pwm_set_gpio_level(GPIO_PWM_SERVO, pulso_inicial); 
+            pwm_set_gpio_level(GPIO_PWM_LED, pulso_inicial); 
             pulso_inicial -= step;
             sleep_ms(10);  // Delay de 10ms para suavizar a movimentação
         }
@@ -59,14 +66,17 @@ int main() {
 
     // Calcula a quantidade de ciclos em nível alto que o sinal PWM deve permanecer para a posição 180° (2400us)
     pwm_set_gpio_level(GPIO_PWM_SERVO, CICLO_180);
+    pwm_set_gpio_level(GPIO_PWM_LED, CICLO_180);
     sleep_ms(5000);
 
     // Calcula a quantidade de ciclos em nível alto que o sinal PWM deve permanecer para a posição 90° (1470us)
     pwm_set_gpio_level(GPIO_PWM_SERVO, CICLO_90);
+    pwm_set_gpio_level(GPIO_PWM_LED, CICLO_90);
     sleep_ms(5000);
 
     // Calcula a quantidade de ciclos em nível alto que o sinal PWM deve permanecer para a posição 0° (500us)
     pwm_set_gpio_level(GPIO_PWM_SERVO, CICLO_0);
+    pwm_set_gpio_level(GPIO_PWM_LED, CICLO_0);
     sleep_ms(5000);
 
     // Movimentação periódica suave entre 0° e 180°
