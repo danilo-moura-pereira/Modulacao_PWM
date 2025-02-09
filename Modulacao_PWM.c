d31883892a1eec3f4eb11e8bcaa7cfff4a112de9
Danilo Moura Pereira <danilo.mourapereira@gmail.com>
2025-02-09 00:37:13 -0300

    Implementação da tarefa - primeiro commit

---
+++ Modulacao_PWM.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/pwm.h"
+
+// O clock PWM foi definido arbitrariamente em 1 MHz para facilitar os cálculos
+#define GPIO_PWM_SERVO  22      // GPIO do servo motor
+#define PWM_FREQUENCY   50      // Frequência definida em 50Hz para que o servo motor funcione
+#define GPIO_PWM_LED    12      // GPIO do LED RGB AZUL
+#define WRAP_PERIOD     20000   // Valor máximo do contador - WRAP (considerando clock PWM em 1 MHz)
+#define PWM_DIVISER     125.0   // Clock do divisor em 125 
+#define STEP_SERVO      5       // Passo do duty cycle do servo motor em 5 milissegundos
+#define CICLO_0         500     // Ciclo ativo para mover o braço do servo motor a 0º
+#define CICLO_90        1470    // Ciclo ativo para mover o braço do servo motor a 180º
+#define CICLO_180       2400    // Ciclo ativo para mover o braço do servo motor a 180º
+
+// Duty cycle inicial
+uint16_t dutyCycle = 200; 
+
+void init_pwm() {
+    // Habilita o pino GPIO como PWM do servo motor 
+    gpio_set_function(GPIO_PWM_SERVO, GPIO_FUNC_PWM);
+    
+    // Captura o canal (slice) PWM do servo motor 
+    uint slicePWM = pwm_gpio_to_slice_num(GPIO_PWM_SERVO);
+
+    // Define o dividor de clock do PWM do servo motor em 2.0
+    pwm_set_clkdiv(slicePWM, PWM_DIVISER);
+
+    // Define o valor WRAP em 2000
+    pwm_set_wrap(slicePWM, WRAP_PERIOD);
+
+    // Habilita o PWM no slice correspondente
+    pwm_set_enabled(slicePWM, 1);
+}
+
+// Movimenta suavemente o servo entre dois pulsos, com incremento de 5us
+void move_servo(uint16_t pulso_inicial, uint16_t pulso_final) {
+    uint16_t step = STEP_SERVO;
+    if (pulso_inicial < pulso_final) {
+        while (pulso_inicial <= pulso_final) {
+            pwm_set_gpio_level(GPIO_PWM_SERVO, pulso_inicial); 
+            pulso_inicial += step;
+            sleep_ms(10);  // Delay de 10ms para suavizar a movimentação
+        }
+    } else {
+        while (pulso_inicial >= pulso_final) {
+            pwm_set_gpio_level(GPIO_PWM_SERVO, pulso_inicial); 
+            pulso_inicial -= step;
+            sleep_ms(10);  // Delay de 10ms para suavizar a movimentação
+        }
+    }
+}
+
+int main() {
+    stdio_init_all();
+
+    // Inicializa PWM
+    init_pwm();
+
+    // Calcula a quantidade de ciclos em nível alto que o sinal PWM deve permanecer para a posição 180° (2400us)
+    pwm_set_gpio_level(GPIO_PWM_SERVO, CICLO_180);
+    sleep_ms(5000);
+
+    // Calcula a quantidade de ciclos em nível alto que o sinal PWM deve permanecer para a posição 90° (1470us)
+    pwm_set_gpio_level(GPIO_PWM_SERVO, CICLO_90);
+    sleep_ms(5000);
+
+    // Calcula a quantidade de ciclos em nível alto que o sinal PWM deve permanecer para a posição 0° (500us)
+    pwm_set_gpio_level(GPIO_PWM_SERVO, CICLO_0);
+    sleep_ms(5000);
+
+    // Movimentação periódica suave entre 0° e 180°
+    while (true) {
+        move_servo(CICLO_0, CICLO_180);   // Movimenta de 0° para 180°
+        move_servo(CICLO_180, CICLO_0);   // Movimenta de 180° para 0°
+    }
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# Modulacao_PWM
-Embarcatech - Capítulo 7 - Aula Síncrona 06/02/2025
+### Link do vídeo explicativo
+https://youtu.be/...
+
+# UART_I2C
+Embarcatech - Tarefa 2 - Aula Síncrona 06/02
+
+### Requisitos cumpridos pelo projeto
+1. Considerando a GPIO 22, defina a sua frequência de PWM para, aproximadamente, 50Hz – período de 20ms. 
+2. Defina o ciclo ativo do módulo PWM para 2.400µs (microssegundos) – Ciclo de Trabalho (Duty Cycle) de 0,12%. Isto ajustará a flange (braço) do servomotor para a posição de, aproximadamente, 180 graus. Aguarde 05 segundos nesta posição. 
+3. Defina o ciclo ativo do módulo PWM para 1.470µs (microssegundos) – Ciclo de Trabalho (Duty Cycle) de 0,0735%. Isto ajustará a flange do servomotor para a posição de, aproximadamente, 90 graus. Aguarde 05 segundos nesta posição. 
+4. Defina o ciclo ativo do módulo PWM para 500µs (microssegundos) – Ciclo de Trabalho (Duty Cycle) de 0,025%. Isto ajustará a flange do servomotor para a posição de, aproximadamente, 0 graus. Aguarde 05 segundos nesta posição. 
+5. Após a realização das proposições anteriores, crie uma rotina para movimentação periódica do braço do servomotor entre os ângulos de 0 e 180 graus. Obs.: a movimentação da flange deve ser suave, recomenda-se o incremento de ciclo ativo de ±5µs, com um atraso de ajuste de 10ms. 
+6. Com o emprego da Ferramenta Educacional BitDogLab, faça um experimento com o código deste exercício utilizando o LED RGB – GPIO 12. O que o discente observou no comportamento da iluminação do referido LED?
+
+### Funcionamento 📋
+1. Utilizando o ambiente de simulação WOKWI, foram incluídos uma placa Raspberry Pico W e 1 servo motor padrão.
+2. O servo motor foi contectado à GPIO 22.
+3. A frequência do módulo PWM foi alterada para, aproximadamente, 50Hz – período de 20ms. 
+4. O ciclo ativo do módulo PWM foi alterado para 2.400µs (microssegundos) – Ciclo de Trabalho (Duty Cycle) de 0,12%. O braço do servo motor foi deslocado, aproximadamente, para 180 graus. 
+5. Foi inserida uma pausa de 5 segundos nesta posição.
+6. O ciclo ativo do módulo PWM foi alterado para 1.470µs (microssegundos) – Ciclo de Trabalho (Duty Cycle) de 0,0735%. O braço do servo motor foi deslocado, aproximadamente, para 90 graus.
+7. Foi inserida uma pausa de 5 segundos nesta posição.
+8. O ciclo ativo do módulo PWM foi alterado para 500µs (microssegundos) – Ciclo de Trabalho (Duty Cycle) de 0,025%. O braço do servo motor foi deslocado, aproximadamente, para 0 graus. 
+9. Foi inserida uma pausa de 5 segundos nesta posição.
+10. Foi criada uma rotina para movimentação periódica do braço do servomotor entre os ângulos de 0 e 180 graus. 
+11. Utilizando a placa BitDogLab, foi feito um experimento com o código anterior utilizando o LED RGB – GPIO 12.
